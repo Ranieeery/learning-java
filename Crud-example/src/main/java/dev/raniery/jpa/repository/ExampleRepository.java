8a1fd1247d6147afe9fcacddc6603913aa9a1e20
Ranieeery <raniery2003@hotmail.com>
2023-08-15 18:13:32 -0300

    feat: add repository class

--- Crud-example/src/main/java/dev/raniery/jpa/repository/ExampleRepository.java
+++ Crud-example/src/main/java/dev/raniery/jpa/repository/ExampleRepository.java
@@ -1,2 +1,9 @@
-package dev.raniery.jpa;public class ExampleRepository {
+package dev.raniery.jpa.repository;
+
+import dev.raniery.jpa.entity.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExampleRepository extends JpaRepository<Example, Long> {
 }
