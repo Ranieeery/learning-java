e7d60ddc6893a6175d8f963ad2414181a26bb942
Ranieeery <raniery2003@hotmail.com>
2023-08-15 18:11:49 -0300

    build: add entity class

--- Crud-example/src/main/java/dev/raniery/jpa/entity/Example.java
+++ Crud-example/src/main/java/dev/raniery/jpa/entity/Example.java
@@ -1,2 +1,21 @@
-package dev.raniery.jpa;public class Example {
+package dev.raniery.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "example")
+public class Example implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
 }
