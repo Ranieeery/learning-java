cf79d08b4ee88c711d0b8340451693ed7d5e2e6a
Ranieeery <raniery2003@hotmail.com>
2023-08-15 18:12:31 -0300

    build: add resources class

--- Crud-example/src/main/java/dev/raniery/jpa/resources/ExampleResources.java
+++ Crud-example/src/main/java/dev/raniery/jpa/resources/ExampleResources.java
@@ -1,7 +1,25 @@
 package dev.raniery.jpa.resources;
 
+import dev.raniery.jpa.DTO.ExampleDTO;
+import dev.raniery.jpa.services.ExampleServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class Resources {
+@RequestMapping(value = "/example")
+public class ExampleResources {
+
+    @Autowired
+    private ExampleServices services;
+
+    @GetMapping
+    public ResponseEntity<List<ExampleDTO>> findAll() {
+        List<ExampleDTO> list = services.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 }
