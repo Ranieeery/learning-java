ac8353c22023590bf5a58eb1ff7d3948a77fc42a
Ranieeery <raniery2003@hotmail.com>
2023-08-15 17:50:42 -0300

    feat: create dto class

--- Crud-example/src/main/java/dev/raniery/jpa/DTO/ExampleDTO.java
+++ Crud-example/src/main/java/dev/raniery/jpa/DTO/ExampleDTO.java
@@ -1,2 +1,18 @@
-package dev.raniery.jpa.DTO;public class ExampleDTO {
+package dev.raniery.jpa.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExampleDTO implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
 }
