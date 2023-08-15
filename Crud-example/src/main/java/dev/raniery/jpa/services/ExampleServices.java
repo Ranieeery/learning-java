e4f0ca2742f42abbf7a4f64b49abdc4075859224
Ranieeery <raniery2003@hotmail.com>
2023-08-15 18:12:15 -0300

    build: add service class

--- Crud-example/src/main/java/dev/raniery/jpa/services/ExampleServices.java
+++ Crud-example/src/main/java/dev/raniery/jpa/services/ExampleServices.java
@@ -1,2 +1,25 @@
-package dev.raniery.jpa;public class ExampleServices {
+package dev.raniery.jpa.services;
+
+import dev.raniery.jpa.DTO.ExampleDTO;
+import dev.raniery.jpa.entity.Example;
+import org.springframework.stereotype.Service;
+import dev.raniery.jpa.repository.ExampleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ExampleServices {
+
+    @Autowired
+    private ExampleRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<ExampleDTO> findAll() {
+        List<Example> list = repository.findAll();
+        return list.stream().map(ExampleDTO::new).collect(Collectors.toList());
+    }
+
 }
