492e539c82d9ca930a8cb633e28853ab53be1b2a
Ranieeery <raniery2003@hotmail.com>
2023-08-15 18:11:23 -0300

    build: add DTO class

--- Crud-example/src/main/java/dev/raniery/jpa/DTO/ExampleDTO.java
+++ Crud-example/src/main/java/dev/raniery/jpa/DTO/ExampleDTO.java
@@ -1,9 +1,10 @@
 package dev.raniery.jpa.DTO;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+import dev.raniery.jpa.entity.Example;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -15,4 +16,8 @@ public class ExampleDTO implements java.io.Serializable {
     private Long id;
     private String name;
 
+    public ExampleDTO(Example entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
 }
